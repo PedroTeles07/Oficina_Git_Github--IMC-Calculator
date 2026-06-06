8eeceb29c41e2afd31d632dc85ee661704196138
Pedro Teles <pedrotelesneto470@gmail.com>
2026-06-06 09:24:38 -0300

    add calculo de imc

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, Git!");
+﻿Console.WriteLine("Digite seu nome:");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua altura(m):");
+double altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite seu peso(kg):");
+double peso = double.Parse(Console.ReadLine());
+
+double imc = 0;
+
+imc = peso/(altura * altura);
+
+Console.WriteLine($"Seu IMC é igual a {imc}");
--- obj/Debug/net10.0/programa.AssemblyInfo.cs
+++ obj/Debug/net10.0/programa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc8a458454700b5bb030b87a0db8d32496089ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("programa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
